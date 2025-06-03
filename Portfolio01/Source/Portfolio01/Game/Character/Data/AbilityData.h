2dae666a7937fa3063a99c6fd17d0c28ffd86e4a
승훈 정 <vcd0h24010@gmail.com>
2025-06-04 07:05:56 +0900

    2025.06.04 | 어빌리티 세팅

--- Portfolio01/Source/Portfolio01/Game/Character/Data/AbilityData.h
+++ Portfolio01/Source/Portfolio01/Game/Character/Data/AbilityData.h
@@ -3,7 +3,7 @@
 #include "CoreMinimal.h"
 #include "Engine/DataTable.h"
 #include "UObject/NoExportTypes.h"
-#include "Game/PPGameMode.h"
+#include "Abilities/GameplayAbility.h"
 #include <Game/Character/CharacterStateEnums.h>
 
 #include "AbilityData.generated.h"
@@ -16,18 +16,8 @@ struct PORTFOLIO01_API FAbilityData : public FTableRowBase
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data") //¾îºô¸®Æ¼Å¸ÀÔ
-		FName TypeName; 
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data") //¾îºô¸®Æ¼
-		TMap<FName, class UGameplayAbility*> Ability;
-
-	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data") //¾îºô¸®Æ¼Å¸ÀÔ
-	//	TMap<EnumType, class UGameplayAbility*> Type;
-	
-	//enum class &Type = EnumTypeMap[TypeName]; // EnumTypeMap is a TMap<FName, EnumType> defined in PPGameMode
-
-	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data") //¾îºô¸®Æ¼
-	//	TMap<Type, class UGameplayAbility*> Ability;
+	/** Ability */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability") 
+		TMap<FName, TSubclassOf<UGameplayAbility> > Ability;
 
 };
